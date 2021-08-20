aae01437fed2af8d32ff303bde5c1c3a2386d20b
sangpm99 <sangpm72@wru.vn>
2021-08-20 11:46:12 +0700

    add index images and styles

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Du học Vision Edu</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="./styles/style-main.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  </head>
+  <body>
+    <!-- Begin Header -->
+    <section class="header">
+        <div class="container">
+            <img src="./images/logo.png" alt="Logo">
+        </div>
+    </section>
+    <!-- End header -->
+      
+    <!-- Begin slide 1-->
+    <section class="slide-1">
+        <div class="container">
+            Slide 1
+        </div>
+    </section>
+    <!-- End slide 1-->
+
+    <!-- Begin address -->
+    <section class="address">
+        <div class="container">
+            Address
+        </div>
+    </section>
+    <!-- End address -->
+
+    <!-- Begin quick contact -->
+    <section class="quick-contact">
+        <div class="container">
+            Quick Contact
+        </div>
+    </section>
+    <!-- End quick contact -->
+
+    <!-- Begin seminor -->
+    <section class="seminor">
+        <div class="container">
+            Seminor
+        </div>
+    </section>
+    <!-- End seminor -->
+
+    <!-- Begin study abroad in japan -->
+    <section class="study-abroad-in-japan">
+        <div class="container">
+            Study abroad in Japan
+        </div>
+    </section>
+    <!-- End study abroad in Japan -->
+
+    <!-- Begin study abroad in korea -->
+    <section class="study-abroad-in-korea">
+        <div class="container">
+            Study abroad in Korea
+        </div>
+    </section>
+    <!-- End study abroad in korea -->
+
+    <!-- Begin study abroad in germany -->
+    <section class="study-abroad-in-germany">
+        <div class="container">
+            Study abroad in Germany
+        </div>
+    </section>
+    <!-- End study abroad in germany -->
+
+    <!-- Begin study abroad in china -->
+    <section class="study-abroad-in-china">
+        <div class="container">
+            Study abroad in China
+        </div>
+    </section>
+
+    <!-- Begin study abroad in taiwan -->
+    <section class="study-abroad-in-taiwan">
+        <div class="container">
+            Study abroad in Taiwan
+        </div>
+    </section>
+    <!-- End study abroad in taiwan -->
+
+    <!-- Begin work at division -->
+    <section class="word-at-division">
+        <div class="container">
+            Work at Division
+        </div>
+    </section>
+    <!-- End word at division -->
+
+    <!-- Begin consulting team -->
+    <section class="consulting-team">
+        <div class="container">
+            Consulting team
+        </div>
+    </section>
+    <!-- End consulting team -->
+
+    <!-- Begin slide 2 -->
+    <section class="slide-2">
+        <div class="container">
+            Slide 2
+        </div>
+    </section>
+    <!-- End slide 2 -->
+
+    <!-- Begin footer -->
+    <section class="footer">
+        <div class="container">
+            Footer
+        </div>
+    </section>
+    <!-- End footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  </body>
+</html>
